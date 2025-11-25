9f0076746a88c0a1f78d68fcb02a7e043fd59863
kelsiecakessugarflowers <kelsie@kelsiecakes.com>
2025-11-25 15:48:16 -0500

    Handle review preview empty state

--- acf-kelsie-review-block.php
+++ acf-kelsie-review-block.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Kelsie ACF Reviews Block
  * Description: ACF block for displaying Reviews repeater content with optional Rank Math schema integration.
- * Version:     1.1.1
+ * Version:     1.1.2
  * Author:      Kelsie Cakes
  */
 
